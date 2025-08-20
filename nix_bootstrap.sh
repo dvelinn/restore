25d74981419c980e6334057c7ba0285a0bea5d72
dvelinn <git@voidgazer.org>
2025-08-20 18:27:04 -0400

    install fixes

--- nix_bootstrap.sh
+++ nix_bootstrap.sh
@@ -6,8 +6,6 @@ REPO_SSH="git@github.com:dvelinn/nixos.git"
 CLONE_DIR="$HOME/.voidgazer"
 # ---------------
 
-nix-shell -p gh
-
 echo "[1/3] GitHub device login (approve on phone)…"
 gh auth login --hostname github.com --git-protocol ssh --web
 
@@ -15,5 +13,5 @@ echo "[2/3] Clone private repo via SSH…"
 gh repo clone "$REPO_SSH"
 mv nixos .voidgazer
 
-echo "[3/3] Done. Using installer within cloned repo to build system."
+echo "[3/3] Done. Using installer to build system."
 exec "$CLONE_DIR/scripts/nix_install.sh"
